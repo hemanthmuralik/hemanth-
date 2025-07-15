7f0964d63fd162abee88c40350b0865b00463449
hemanthmuralik <hemanthmuralikattur@gmail.com>
2025-07-15 14:00:36 +0530

    Add files via upload

---
+++ sundy.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+ int option;
+printf("age of a person");
+ scanf("%d",&option);
+
+ switch(option)
+ {
+   case 1 :
+   printf("sunday"); 
+      break;
+  case 2 :
+  printf("monday");
+  break;
+   case 3 :
+   printf("tuesday");
+   break;
+    case 4 :
+    printf("wednesday");
+    break;
+     case 5 :
+     printf("thursday");
+     break;
+     case 6 :
+     printf("friday");
+     break;
+     case 7 :
+     printf("saturday");
+     default :
+     break;
+}
+}
+
